4086248fe14f6b778f5b53fe77996359bacaa316
khaanh32 <137624125+khaanh32@users.noreply.github.com>
2026-03-14 16:14:28 +0700

    Configure Vite server proxy for API calls
    Add proxy configuration for API requests to Render.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Bất cứ khi nào Frontend gọi '/api', Vite sẽ tự động gửi ngầm nó sang Render
+      '/api': {
+        target: 'https://webchieut6-1.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
